9c5fc714d1ef99aa67e5c4ccc9b55c5843f4bd60
yukikaze <xuyang2003xuyang@outlook.com>
2024-11-23 23:16:48 +0800

    添加 SOR 和 Gauss 解法程序，更新接口模块

---
+++ u1/bigtosmall__genmod.f90
@@ -0,0 +1,10 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sat Nov 23 23:15:38 2024
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE BIGTOSMALL__genmod
+          INTERFACE 
+            SUBROUTINE BIGTOSMALL(N)
+              INTEGER(KIND=8), INTENT(IN) :: N
+            END SUBROUTINE BIGTOSMALL
+          END INTERFACE 
+        END MODULE BIGTOSMALL__genmod
---
+++ u1/smalltobig__genmod.f90
@@ -0,0 +1,10 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sat Nov 23 23:15:38 2024
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE SMALLTOBIG__genmod
+          INTERFACE 
+            SUBROUTINE SMALLTOBIG(N)
+              INTEGER(KIND=8), INTENT(IN) :: N
+            END SUBROUTINE SMALLTOBIG
+          END INTERFACE 
+        END MODULE SMALLTOBIG__genmod
